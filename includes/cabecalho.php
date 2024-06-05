14a4164d1d25db3aed9c8a7b15bb00637b06155d
EnzoMiquelini <enzo.smiquelini@gmail.com>
2024-06-05 00:29:35 -0300

    continue

---
+++ includes/cabecalho.php
@@ -0,0 +1,22 @@
+<header>
+    <nav class="cabecalhoNav">
+        <div class="logo">
+            <img src="../img/01.-LOGOTIPO-CORRETO.png" alt="logo">
+        </div>
+        <div class="menu">
+            <a href="">Home</a>
+            <a href="">Espécie</a>
+            <a href="">Contato </a>
+        </div>
+    </nav>
+    <div class="headerEscritaFundo">
+        <div class="headerEscrita">
+            <div class="headerH1">
+                <h1>CARPA NISHIKIGOI/KOI</h1>
+            </div>
+            <div class="headerP">
+                <p>Possuindo características semelhantes a Carpa Colorida, a Carpa Koi difere-se por apresentar colorações específicas e raras, podendo ser espelhadas, de escamas ou de couro. Esta espécie é considerada como exclusiva, possuindo alto valor agregado</p>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+
+include __DIR__.'/includes/header.php';
+include __DIR__.'/includes/cabecalho.php';
+include __DIR__.'/includes/rodape.php';
+include __DIR__.'/includes/footer.php';
+
+?>
